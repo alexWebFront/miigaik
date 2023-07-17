15f094ca5301e17931e00614a55f356f72b01796
Alexey <skejing@yandex.ru>
2023-07-17 22:29:53 +0400

    Добавил скрипты на мобильное меню и сортировку

--- webpack-clear/src/index.js
+++ webpack-clear/src/index.js
@@ -22,3 +22,63 @@ import './styles/index.scss'
 //app.append(logo, section)
 console.log(window)
 document.cookie = "user=John; domain=http://217.67.179.251/"
+
+const menuLink = document.querySelectorAll(".header__mobile-menu-img");
+const sideMenuActive = document.querySelectorAll('.side-menu');
+const tableShowText = document.querySelectorAll('.table__body-item-show-text')
+const dropDownFolder = document.querySelectorAll('.dropdown-folder')
+const tableSortAction = document.querySelectorAll('.dropdown__select-mobile')
+
+// Сортировка по таблице
+tableSortAction.forEach((itemSort) => {
+
+  itemSort.addEventListener('click', () => {
+    const tableSort = document.querySelectorAll('.dropdown__select-mobile-active')
+    tableSort.forEach((itemToggle) => {
+      itemToggle.classList.add('active')
+
+      const tableSortClose = document.querySelector('.dropdown__select-mobile-active-cancel')
+      const tableSortCloseCross = document.querySelector('.dropdown__select-mobile-active-cross svg')
+
+      tableSortCloseCross.addEventListener('click', () => {
+        itemToggle.classList.remove('active')
+      })
+
+      tableSortClose.addEventListener('click', () => {
+        itemToggle.classList.remove('active')
+      })
+    })
+  })
+})
+
+// Сортировка с названием акселератора
+dropDownFolder.forEach((itemFolder) => {
+  const dropDownFolderSelect = document.querySelector('.dropdown-folder__select')
+  dropDownFolderSelect.addEventListener('click', () => {
+    itemFolder.classList.toggle('active')
+  })
+})
+
+
+// Свернуть/показать полностью - не сделано
+tableShowText.forEach((itemShow) => {
+  itemShow.addEventListener('click', () => {
+    console.log(13)
+  })
+})
+
+// Мобильное меню
+menuLink.forEach((itemLink) => {
+  itemLink.addEventListener('click', () => {
+
+    sideMenuActive.forEach((itemActive) => {
+      itemActive.classList.add('active')
+
+      const sideMenuClose = document.querySelector('.side-menu__info-close');
+
+      sideMenuClose.addEventListener('click', () => {
+        itemActive.classList.remove('active')
+      })
+    })
+  })
+})
